09545880084d580d7e4abb8dcbac7158e1adeba5
AbramsEA <abramson.ea@gmail.com>
2020-07-30 16:10:01 +0300

    slider-almost-done

--- src/js/about.js
+++ src/js/about.js
@@ -1,2 +1,28 @@
 import "../pages/about.css";
 import "../../node_modules/normalize.css/normalize.css"
+
+import Swiper from 'swiper/bundle';
+import 'swiper/swiper-bundle.css';
+
+const mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    loop: false,
+    speed: 500,
+    slidesPerView: 3,
+    spaceBetween: 16,
+    uniqueNavElements: true,
+    slidesOffsetBefore: 104,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    scrollbar: false,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  })
--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,4 @@
 import "../pages/index.css";
 import "../../node_modules/normalize.css/normalize.css"
 
-import Swiper from 'swiper/bundle';
-import 'swiper/swiper-bundle.css';
-
-const mySwiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  })
 
